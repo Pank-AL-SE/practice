2e695e5c58e7a478185b78535c2cbdc1c9948ed2
USER <sasha@BUBUNTUU.asd>
2024-03-05 15:10:40 +0700

    ready openstreetmap

--- practice/openstreetmap/test_second_withparams.py
+++ practice/openstreetmap/test_second_withparams.py
@@ -1,10 +1,13 @@
 import pytest
 import random
 import pytest_check 
+import allure_pytest
+import allure
 from first.testing_api import *
 from lib_generators import *
 
-class TestAPI:  
+
+class TestAPI: 
     @pytest.mark.parametrize("lat,lon,res", [(generate_invalid_params(),generate_invalid_params(), 400),#
                                              (generate_invalid_params(),generate_invalid_params(), 400),#                                         
                                              ('asdas','asdasd', 400),#
@@ -36,11 +39,11 @@ class TestAPI:
 
     @pytest.mark.parametrize("input_addr,input_city",[('\u0443\u043b\u0438\u0446\u0430\u0020\u0434\u043e\u0431\u0440\u043e\u043b\u044e\u0431\u043e\u0432\u0430\u0020\u0032\u0430','НоВоСибирск'),
                                                       ('630001','НоВоСибирск'),
-                                                      ('','Алтай')])
+                                                      ('','123')])
     def test_valid_addr(self,input_addr,input_city):
         a,b = 55.0025574, 82.9422944
         res = functions.find_street(input_addr,input_city)
-        c,d = float(res[0]),float(res[1])
+        c,d = float(res[0]),float(res[1])        
         pytest_check.equal(  abs(a-c) <= 0.01, abs(b-d) <= 0.01)
     
     
