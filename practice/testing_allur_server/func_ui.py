39eb3020df422eb94a2c81f267555bf9c3a4c4fe
Pank_AL_SE <pank_al_se@mail.ru>
2024-05-17 16:55:14 +0700

    cool selenium

--- practice/testing_allur_server/func_ui.py
+++ practice/testing_allur_server/func_ui.py
@@ -21,6 +21,17 @@ def init_max_window():
     except:
         return(404)
     
+def init_project():
+    chrome_options = Options()
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--start-maximized")
+    driver = webdriver.Chrome(options=chrome_options)
+    try:
+        driver.get("http://localhost:5252/allure-docker-service-ui/projects/2")
+        return driver
+    except:
+        return(404)
+    
 def init_half_window():
     chrome_options = Options()
     chrome_options.add_argument("--disable-extensions")
@@ -52,6 +63,7 @@ def check_slide_panel():
     time.sleep(5)
     btns[0].click()
     time.sleep(5)
+    print(slide_pannel[0].get_attribute('outerHTML'))
     if 'style="visibility: hidden; transform: translateX(-240px);"' not in slide_pannel[0].get_attribute('outerHTML'):
         return True
     else:
@@ -59,6 +71,8 @@ def check_slide_panel():
 
 def check_theme():
     driver = init_max_window()
+    if driver == 404:
+        return "Not Found"
     header = driver.find_elements(By.XPATH, '//div/div/div')
     style1 = str(header[0].get_attribute('outerHTML'))[:19]
     switcher = driver.find_elements(By.XPATH, '//button')
@@ -75,6 +89,8 @@ def check_theme():
     
 def check_language():
     driver = init_max_window()
+    if driver == 404:
+        return "Not Found"
     switcher = driver.find_elements(By.XPATH, '//button')
     switcher[7].click()    
     time.sleep(5)
@@ -86,8 +102,12 @@ def check_language():
     
 def check_window():
     driver = init_half_window()
+    if driver == 404:
+        return "Not Found"
     header1 = driver.find_elements(By.XPATH, '//button')
     driver = init_max_window()
+    if driver == 404:
+        return "Not Found"
     header2 = driver.find_elements(By.XPATH, '//button')
     try:
         header1[6].click()
@@ -98,7 +118,55 @@ def check_window():
     except:
         return 'Unknown Button'
     return res
+
+
+def check_create_project():
+    driver = init_max_window()
+    if driver == 404:
+        return "Not Found"
     
-def send_file():
-    pass
-print(check_language())
+    header = driver.find_elements(By.XPATH, '//button')
+    time.sleep(5)
+    header[4].click()
+    time.sleep(5)
+    header1 = driver.find_elements(By.XPATH, '//input')
+    header1[3].send_keys("2")
+    time.sleep(5)
+    header = driver.find_elements(By.XPATH, '//button')
+    cnt = 0
+    # for i in range(len(header)):
+    #     print(str(header[i].get_attribute('outerHTML')))
+    #     if '<span class="MuiButton-label">Create</span><span class="MuiTouchRipple-root"></span>' in header[i].get_attribute('outerHTML'):
+    #         cnt+=1
+    # print(cnt)
+    header[16].click()
+    time.sleep(5)
+
+def check_send_file():
+    driver = init_project()
+    if driver == 404:
+        return "Not Found"
+    header = driver.find_elements(By.XPATH, '//button')
+    # cnt = 0
+    # for i in range(len(header)):
+    #     print(str(header[i].get_attribute('outerHTML')))
+    #     if 'Send Results' in header[i].get_attribute('outerHTML'):
+    #         print(i)
+    # print(cnt)
+    header[16].click()
+    time.sleep(5)
+    header = driver.find_elements(By.XPATH, '//label')
+    # cnt = 0
+    # for i in range(len(header)):
+    #     print(str(header[i].get_attribute('outerHTML')))
+    #     if 'Upload Files' in header[i].get_attribute('outerHTML'):
+    #         print(i)
+    # print(cnt)
+    header[0].click()
+    time.sleep(5)
+    header[0].send_keys("invalid_res/test.txt")
+    time.sleep(5)
+
+
+
+print(check_send_file())
